a31a7c6848f2ecebc3195b0c27aa7affc49cf7a3
tmugnaz <tmugnai@gmail.com>
2023-11-07 16:27:48 +0100

    edts;

--- project5/bho2.java
+++ project5/bho2.java
@@ -1,12 +1,23 @@
 package project5;
 
 public class bho2 {public static void main(String[] args) {
-    String[] bho = new String[6];
-    for(int i=0;i<6;i++){
+     String parola="oidocrop";
+     System.out.println(toReverse(parola));
+        
+     
        
     }
     
 
-}
+
+public static String toReverse(String parola){
+    String reverso="";
+    for(int i=parola.length()-1;i>=0;i--){
+        reverso = reverso+ parola.charAt(i);
+
+    }
+    return reverso;}
     
 }
+    
+
